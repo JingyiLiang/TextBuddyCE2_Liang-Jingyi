a198632252eb67d674249fcc5d742b5fae20860d
JingyiLiang <jingyi_zoe@hotmail.com>
2015-02-27 01:04:54 +0800

    2-file

--- TB_CE2/TB_CE2/TextBuddy.cpp
+++ TB_CE2/TB_CE2/TextBuddy.cpp
@@ -1,17 +1,154 @@
 #include "TextBuddy.h"
 
+using namespace std;
 
-TextBuddy::TextBuddy(void){}
 
-TextBuddy::TextBuddy(int num){
-    _lineNumber = num;
-    return;
+const string MESSAGE_WELCOME = "Wellcome to TextBuddy. %s is ready for use";
+const string MESSAGE_TERMINATION = "Enter any key to exit:";
+const string WARNING_INVALID_COMMAND_ENTERED = "Invalid command: %s";
+const string WARNING_COMMAND_LINE_PARAMETER_INPUT_ERROR = "ERROR: Usage: TextBuddy.exe filename.txt";
+const string MESSAGE_ADDED = "added to %s: \"%s\"";
+const string MESSAGE_DELETED ="deleted from %s: \"%s\"";
+const string MESSAGE_EMPTY = "%s is empty";
+const string MESSAGE_CLEARED_ALL = "all content deleted from %s";
+const string MESSAGE_EXIT = "exit";
+const string ERROR_UNRECOGNISED_COMMAND_TYPE = "ERROR: Unrecognised command type";
+const string PROMPT_COMMAND =  "command: ";
+
+char buffer[300] = "";
+
+int main(int argc, char* argv[]){
+	//command line parameter usage
+	string filename;
+	TextBuddy tb;
+
+	tb.checkCLI(argc);
+	filename = argv[1];
+	
+	tb.run(filename);
+	return 0;
 }
 
+void TextBuddy::run(string filename){
+
+	//show welcome message
+	sprintf_s(buffer, MESSAGE_WELCOME.c_str(),filename.c_str());
+	showToUser(buffer);
+
+	//always loop untill user enter "exit" command
+	while(true){
+		cout << PROMPT_COMMAND;
+
+		string userCommand;
+		getline(cin,userCommand);
+		string feedback = executeCommand(filename, userCommand);
+		showToUser(feedback);
+	}
+}
+
+void TextBuddy::checkCLI(int argc){
+	if(argc != 2){
+		showToUser(WARNING_COMMAND_LINE_PARAMETER_INPUT_ERROR);
+		showToUser(MESSAGE_TERMINATION);
+		getchar();
+		exit(0);
+	}
+}
+
+string TextBuddy::executeCommand(string filename, string userCommand){
+	CommandType command;
+	string content;
+	string deletedLine;
+
+	command = determineCommandType(getFirstWord(userCommand));
+	content = removeFirstWord(userCommand);
+
+	switch(command){
+	case Add:
+		addLine(content);
+		sprintf_s(buffer, MESSAGE_ADDED.c_str(), filename.c_str(),content.c_str());
+		return buffer;
+	case Display:
+		if(_lineList.begin() == _lineList.end()){
+			sprintf_s(buffer, MESSAGE_EMPTY.c_str(), filename.c_str());
+			return buffer;
+		}
+		else{
+			displayAll();
+			return "";
+		}
+	case Delete:
+		deletedLine = deleteLine(content);
+		sprintf_s(buffer, MESSAGE_DELETED.c_str(), filename.c_str(), deletedLine.c_str());
+		return buffer;
+	case Clear:
+		clearAll();
+		sprintf_s(buffer, MESSAGE_CLEARED_ALL.c_str(),filename.c_str());
+		return buffer;
+	case Exit:
+		writeToFile(filename);
+		sprintf_s(buffer, MESSAGE_EXIT.c_str());
+		showToUser(buffer);
+		exit(0);
+	case Invalid:
+		sprintf_s(buffer, WARNING_INVALID_COMMAND_ENTERED.c_str(), userCommand.c_str());
+		return buffer;
+	default:
+		showToUser(ERROR_UNRECOGNISED_COMMAND_TYPE);
+		showToUser(MESSAGE_TERMINATION); 
+		getchar(); 
+		exit(0); 
+	}
+}
+
+void TextBuddy:: writeToFile(string filename){
+	ofstream ofs;
+	string content;
+	content = allToString();
+	
+	ofs.open(filename);
+	ofs << content << endl;
+	ofs.close();
+
+	return;
+}
+
+string TextBuddy::removeFirstWord(string userCommand){
+	return userCommand.substr(userCommand.find_first_of(" ")+1);
+}
+
+string TextBuddy::getFirstWord(string userCommand){
+	return userCommand.substr(0, userCommand.find(' '));
+	//(?)from microsoft VS online resource forum
+}
+
+TextBuddy::CommandType TextBuddy::determineCommandType(string firstWord){
+	// allow natural typing variations
+	transform(firstWord.begin(), firstWord.end(),firstWord.begin(), ::tolower);
+	
+	if (firstWord == "add"){
+		return CommandType::Add;
+	}
+	else if (firstWord == "display"){
+		return CommandType::Display;
+	}
+	else if (firstWord == "delete"){
+		return CommandType::Delete;
+	}
+	else if (firstWord == "exit"){
+		return CommandType::Exit;
+	}
+	else if(firstWord == "clear"){
+		return CommandType::Clear;
+	}
+	else return CommandType::Invalid;
+}
+
+void TextBuddy::showToUser(string content){
+	cout << content << endl;
+}
 void TextBuddy::addLine(string content){
     _lineList.push_back(content);
-    _lineNumber++;
-    _lineNumber = _lineNumber + 1;
     return;
 }
 
@@ -29,7 +166,6 @@ string TextBuddy::deleteLine(string lineNum){
 
 void TextBuddy::clearAll(){
     _lineList.clear();
-    _lineNumber = 0;
     return;
 }
 
@@ -42,10 +178,6 @@ void TextBuddy::displayAll(){
         n++;
     }
     return;
-}
-
-int TextBuddy::getLineNum(){
-    return _lineNumber;
 }
 
 string TextBuddy::allToString(){
@@ -61,5 +193,4 @@ string TextBuddy::allToString(){
     return oss.str();
 }
 
-TextBuddy::~TextBuddy(void){}
 
--- TB_CE2/TB_CE2/TextBuddy.h
+++ TB_CE2/TB_CE2/TextBuddy.h
@@ -3,10 +3,14 @@
 #ifndef TEXTBUDDY_H
 #define TEXTBUDDY_H
 
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
+#include <algorithm>
 #include <list>
+#include "TextBuddy.h"
 
 using namespace std;
 
@@ -14,19 +18,38 @@ class TextBuddy
 {
 private:
 	list<string> _lineList;
-	int _lineNumber;
 
+	const string MESSAGE_WELCOME;
+	const string MESSAGE_TERMINATION;
+	const string WARNING_INVALID_COMMAND_ENTERED;
+	const string WARNING_COMMAND_LINE_PARAMETER_INPUT_ERROR;
+	const string MESSAGE_ADDED;
+	const string MESSAGE_DELETED;
+	const string MESSAGE_EMPTY;
+	const string MESSAGE_CLEARED_ALL;
+	const string MESSAGE_EXIT;
+	const string ERROR_UNRECOGNISED_COMMAND_TYPE;
+	const string PROMPT_COMMAND;
+	char buffer[300];
 public:
-	TextBuddy(void);
-	TextBuddy(int);
+
 	void addLine(string);
 	string deleteLine(string);
 	void clearAll();
 	void displayAll();
-	int getLineNum();
 	string allToString();
-	~TextBuddy(void);
 
+	enum CommandType {Add = 0, Display = 1, Delete = 2, Clear =3, Exit = 4, Invalid = 5};
+
+	CommandType determineCommandType(string firstWord);
+	string removeFirstWord(string userCommand);
+	string getFirstWord(string userCommand);
+	string executeCommand(string filename, string userCommand);
+
+	void run(string filename);
+	void showToUser(string content);
+	void writeToFile(string filename);
+	void checkCLI(int argc);
 };
 
 #endif
